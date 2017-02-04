63857163b43e1c4159a4bbc83ac8245e467b2aa1
Brian Bycynski <high5bri@hotmail.com>
2017-02-04 06:50:34 -0500

    minor changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,6 +5,7 @@ export default class Contact extends Component {
     return <div>
       <h1>Contact</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <img src='http://www.dogbreedinfo.com/images24/BeagleBayleePurebredDogs8Months1.jpg' />
     </div>
   }
 }
