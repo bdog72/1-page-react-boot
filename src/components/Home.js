dbd33efef1738c48ad8761a87b45a93238bc2f39
Brian Bycynski <high5bri@hotmail.com>
2017-02-04 08:05:25 -0500

    minor changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ export default class Home extends Component {
     return <div>
       <h1>Home</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <img src='http://d21vu35cjx7sd4.cloudfront.net/dims3/MMAH/thumbnail/645x380/quality/90/?url=http%3A%2F%2Fs3.amazonaws.com%2Fassets.prod.vetstreet.com%2F75%2F371580ca7a11e0ad9e12313817c323%2Ffile%2FBeagle-1-645mk062311.jpg' />
+      <img className='pageImages' src='http://d21vu35cjx7sd4.cloudfront.net/dims3/MMAH/thumbnail/645x380/quality/90/?url=http%3A%2F%2Fs3.amazonaws.com%2Fassets.prod.vetstreet.com%2F75%2F371580ca7a11e0ad9e12313817c323%2Ffile%2FBeagle-1-645mk062311.jpg' />
     </div>
   }
 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import { Link } from 'react-router'
-
+// import { LinkContainer } from 'react-router-bootstrap'
+// import { Button } from 'react-bootstrap'
 export default class Layout extends Component {
 
   static propTypes = {
@@ -10,6 +11,15 @@ export default class Layout extends Component {
     return <div>
       <nav>
         <ul>
+          {/* <LinkContainer to={ pathname: '/' }>
+            <Button bsSize='large' />
+          </LinkContainer>
+          <LinkContainer>
+            <Button bsSize='large' />
+          </LinkContainer>
+          <LinkContainer>
+            <Button bsSize='large' />
+          </LinkContainer> */}
           <li><Link to='/'>Home</Link></li>
           <li><Link to='/Contact'>Contact</Link></li>
           <li><Link to='/About'>About</Link></li>
