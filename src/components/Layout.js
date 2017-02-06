59e7c1699d58fe428965b499efd3ecee42fdba4b
Brian Bycynski <high5bri@hotmail.com>
2017-02-06 07:20:20 -0500

    added new dropdown menu using semantic ui

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import { Link } from 'react-router'
-import { Menu } from 'semantic-ui-react'
+import { Menu, Dropdown } from 'semantic-ui-react'
 export default class Layout extends Component {
 
   static propTypes = {
@@ -9,17 +9,26 @@ export default class Layout extends Component {
   render () {
     return <div>
       <nav>
+        {/* semantic-ui-react Links */}
+        <Menu className='menu1'>
+          <Menu.Item as={Link} to='/'>Home</Menu.Item>
+          <Menu.Item as={Link} to='/Contact'>Contact</Menu.Item>
+          <Menu.Item as={Link} to='/About'>About</Menu.Item>
+        </Menu>
         <ul>
-          {/* semantic-ui-react Links */}
-          <Menu>
-            <Menu.Item as={Link} to='/'>Home</Menu.Item>
-            <Menu.Item as={Link} to='/Contact'>Contact</Menu.Item>
-            <Menu.Item as={Link} to='/About'>About</Menu.Item>
-          </Menu>
           <li><Link to='/'>Home</Link></li>
           <li><Link to='/Contact'>Contact</Link></li>
           <li><Link to='/About'>About</Link></li>
         </ul>
+        <Menu vertical>
+          <Dropdown item text='Pages'>
+            <Dropdown.Menu>
+              <Dropdown.Item as={Link} to='/'>Home</Dropdown.Item>
+              <Dropdown.Item as={Link} to='/Contact'>Contact</Dropdown.Item>
+              <Dropdown.Item as={Link} to='/About'>About</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Menu>
       </nav>
       <main>{this.props.children}</main>
     </div>
