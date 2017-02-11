9a4526f658555428c02c100504e4e81c76a12045
Brian Bycynski <high5bri@hotmail.com>
2017-02-11 11:21:22 -0500

    minor changes

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,8 @@ import React, {Component} from 'react'
 import {browserHistory, Router, Route} from 'react-router'
 import Layout from './Layout'
 import Home from './Home'
-// import Seafood from './Seafood'
-// import Burgers from './Burgers'
-// import Wings from './Wings'
-// import Pizza from './Pizza'
+import About from './About'
+import Contact from './Contact'
 
 export default class App extends Component {
 
@@ -14,10 +12,8 @@ export default class App extends Component {
     return <Router history={browserHistory}>
       <Route component={Layout}>
         <Route path='/' component={Home} />
-        <Route path='/Seafood' component={Seafood} />
-        <Route path='/About' component={Burgers} />
-        <Route path='/About' component={Wings} />
-        <Route path='/About' component={Pizza} />
+        <Route path='/About' component={About} />
+        <Route path='/Contact' component={Contact} />
       </Route>
     </Router>
   }
