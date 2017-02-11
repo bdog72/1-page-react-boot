fd3eafb6f695c46f57d5eb0aaff52b3bf5e3f709
Brian Bycynski <high5bri@hotmail.com>
2017-02-11 10:53:57 -0500

    broken-needs to be fixed

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,21 @@ import React, {Component} from 'react'
 import {browserHistory, Router, Route} from 'react-router'
 import Layout from './Layout'
 import Home from './Home'
-import Contact from './Contact'
-import About from './About'
+// import Seafood from './Seafood'
+// import Burgers from './Burgers'
+// import Wings from './Wings'
+// import Pizza from './Pizza'
+
 export default class App extends Component {
 
   render () {
     return <Router history={browserHistory}>
       <Route component={Layout}>
         <Route path='/' component={Home} />
-        <Route path='/Contact' component={Contact} />
-        <Route path='/About' component={About} />
-
+        <Route path='/Seafood' component={Seafood} />
+        <Route path='/About' component={Burgers} />
+        <Route path='/About' component={Wings} />
+        <Route path='/About' component={Pizza} />
       </Route>
     </Router>
   }
